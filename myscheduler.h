f729c7c380ac63bbcbe8235d700c8f3ba7caadc1
jinliu9508 <jinliu9508@gmail.com>
2017-02-26 16:35:30 -0500

    Update myscheduler.h

--- myscheduler.h
+++ myscheduler.h
@@ -22,24 +22,26 @@ struct CPUStatus {
 	unsigned int id;					// id of CPU
 	bool isBusy;						// is there thread is processing in this CPU
 	unsigned int proccessedTid;			// if there is, what is the id of the thread
-
-	CPUStatus(unsigned int num_cpu) {
-		for (int i = 0; i < num_cpu; i++) {
-
-		}
-	}
 };
 
 
 class MyScheduler: public Scheduler {
 public:
-	MyScheduler(Policy p, unsigned int n) : Scheduler(p, n) {}
+	MyScheduler(Policy p, unsigned int n) : Scheduler(p, n) {
+		// initialize the data structure
+		threadList = NULL;
+		for (int i = 0; i < num_cpu; i++) {
+			CPUList[i].id = i;
+			CPUList[i].isBusy = false;
+			CPUList[i].proccessedTid = 0;
+		}
+	}
 	bool Dispatch() override; //Function to implement scheduling policy and to keep a check on processed threads
 	void CreateThread(int arriving_time, int remaining_time, int priority, int tid) override; //Function to create threads and insert them in student defined data structure
 
 	//Declare additional methods(s) below if needed.
 	ThreadsStatus *threadList = NULL;					// declare and initialize the thread list
-	CPUStatus *CPUList;
+	CPUStatus *CPUList = new CPUStatus[num_cpu];
 	void addThreadToList(ThreadDescriptorBlock*);		// add a thread to the list
 	void removeThreadFromList(unsigned int tid);
 };
