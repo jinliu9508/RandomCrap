230c4a606fccad8257509869b038497b28179ed7
jinliu9508 <jinliu9508@gmail.com>
2017-02-27 22:52:45 -0500

    data structure is changed
        Use Vector from library instead of linked list for ease of programming.

--- myscheduler.cpp
+++ myscheduler.cpp
@@ -2,18 +2,22 @@
 /*Define all the functions in 'myschedule.h' here.*/
 #include "myscheduler.h"
 
+// constructor
 MyScheduler::MyScheduler(Policy p, unsigned int n) : Scheduler(p, n) {
 	// initialize the data structure
-	threadList = NULL;
-	for (int i = 0; i < num_cpu; i++) {
+	threadVector.clear();
+	CPUList = new CPUStatus[num_cpu];
+	for (unsigned int i = 0; i < num_cpu; i++) {		// clear out the array of CPU indicator
 		CPUList[i].id = i;
 		CPUList[i].isBusy = false;
 		CPUList[i].proccessedTid = 0;
 	}
 }
 
+// destructor
 MyScheduler::~MyScheduler() {
-
+	threadVector.clear();
+	delete CPUList;
 }
 
 void MyScheduler::CreateThread(int arriving_time, int remaining_time, int priority, int tid) //Thread ID not Process ID
@@ -22,32 +26,36 @@ void MyScheduler::CreateThread(int arriving_time, int remaining_time, int priori
 	//defined data structure
 
 	// create a thread using passed in values
-	ThreadDescriptorBlock *t;
-	t->arriving_time = arriving_time;
-	t->remaining_time = remaining_time;
-	t->priority = priority;
-	t->tid = tid;
+	ThreadDescriptorBlock t;
+	t.arriving_time = arriving_time;
+	t.remaining_time = remaining_time;
+	t.priority = priority;
+	t.tid = tid;
 
-	// add newly created thread to the list
-	addThreadToList(t);
+	// add newly created thread to the vector
+	ThreadsStatus thread;
+	thread.thread = &t;
+	thread.isScheduled = false;
+	thread.CPU_id = 0;
+	thread.next = NULL;
+
+	threadVector.push_back(thread);
 }
 
 bool MyScheduler::Dispatch()
 {
-	//Todo: Check and remove finished threads
-	ThreadsStatus *iterator = threadList;
-	ThreadDescriptorBlock *thr;
-	while (iterator != NULL) {
-		thr = iterator->thread;
-		if (thr->remaining_time == 0) {
-			removeThreadFromList(thr->tid);
-		}
-	}
-
 	//Todo: Check if all the threads are finished; if so, return false
-	if (threadList == NULL)
+	if (threadVector.empty())
 		return false;
 
+	//Todo: Check and remove finished threads
+	vector<ThreadsStatus> iterator = threadVector.begin;
+	// loop through the vector
+	for (auto it = begin(threadVector); it != end(threadVector); ++it) {
+		if (it->thread->remaining_time == 0) {
+			threadVector.erase(it);		// delete if a thread has been processes
+		}
+	}
 
 	switch (policy)
 	{
@@ -70,27 +78,3 @@ bool MyScheduler::Dispatch()
 	return true;
 }
 
-void MyScheduler::addThreadToList(ThreadDescriptorBlock *t) {
-	// initialize the list if empty
-	if (threadList == NULL) {
-		threadList = new ThreadsStatus;
-		threadList->thread = t;
-		threadList->isScheduled = false;
-		threadList->CPU_id = 0;
-		threadList->next = NULL;
-	}
-	else {		// add the thread to the end
-		ThreadsStatus *iterator = threadList;
-		while (iterator != NULL)
-			iterator = iterator->next;
-		iterator->thread = t;
-		iterator->isScheduled = false;
-		iterator->CPU_id = 0;
-		iterator->next = NULL;
-	}
-}
-
-// remove thread in the threadlist that has id of tid
-void MyScheduler::removeThreadFromList(unsigned int tid) {
-
-}
