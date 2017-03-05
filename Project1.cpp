03a382f2cba07722135526862df3517953315cb3
kevinj17 <kevinjiang17@gmail.com>
2017-03-05 04:58:34 -0500

    Update Project1.cpp

--- Project1.cpp
+++ Project1.cpp
@@ -13,9 +13,13 @@ int main(int argc, char* argv[])
 	int thread_num = 5;	// number of thread to be scheduled
 
 	MyScheduler scheduler(policy, CPU_num);
-
+	
+	//intializing variables:
 	int randomRemainingTime = 0;
 	int randomPriority = 0;
+	
+	srand(time(0)); //seeds rand() so it gives random # every time
+	
 	// create thread
 	for (int i = 0; i < thread_num; i++) {
 		randomPriority = rand() % thread_num;		// random int from 0 to thread_num -1
@@ -31,20 +35,25 @@ int main(int argc, char* argv[])
 	case FCFS:
 		cout << "FCFS ";
 		break;
+			
 	case STRFwoP:
 		cout << "STRFwoP ";
 		break;
+			
 	case STRFwP:
 		cout << "STRFwP ";
 		break;
+			
 	case PBS:
 		cout << "PBS " << endl;
 		break;
+			
 	default:
 		cout << "Invalid policy!";
 		throw 0;
 		break;
 	}
+	
 	cout << "running result: " << endl;
 	scheduler.Go();
 
