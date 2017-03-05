a0f52c2e297e68afd40a0f4cdf491c15efd03d75
jinliu9508 <jinliu9508@gmail.com>
2017-03-05 13:02:20 -0500

    Update Project1.cpp

--- Project1.cpp
+++ Project1.cpp
@@ -1,25 +1,26 @@
-// via : Jinwei Liu
+// via : Jinwei Liu, Ghunley Zhang, Kevin Jiang
 
 #include "myscheduler.h"
 #include <iostream>
 #include <stdlib.h>     /* srand, rand */
+#include <time.h>
 
 int main(int argc, char* argv[])
 {
-	cout << "Test bench via: Jinwei Liu\n";
+	cout << "Test bench via: Jinwei Liu, Ghunley Zhang, Kevin Jiang\n";
 
 	Policy policy = PBS;			// replace policy: FCFS, STRFwoP, STRFwP, PBS
-	unsigned int CPU_num = 2;		// number of CPU
+	unsigned int CPU_num = 3;		// number of CPU
 	int thread_num = 5;	// number of thread to be scheduled
 
 	MyScheduler scheduler(policy, CPU_num);
-	
+
 	//intializing variables:
 	int randomRemainingTime = 0;
 	int randomPriority = 0;
-	
+
 	srand(time(0)); //seeds rand() so it gives random # every time
-	
+
 	// create thread
 	for (int i = 0; i < thread_num; i++) {
 		randomPriority = rand() % thread_num;		// random int from 0 to thread_num -1
@@ -35,25 +36,25 @@ int main(int argc, char* argv[])
 	case FCFS:
 		cout << "FCFS ";
 		break;
-			
+
 	case STRFwoP:
 		cout << "STRFwoP ";
 		break;
-			
+
 	case STRFwP:
 		cout << "STRFwP ";
 		break;
-			
+
 	case PBS:
 		cout << "PBS " << endl;
 		break;
-			
+
 	default:
 		cout << "Invalid policy!";
 		throw 0;
 		break;
 	}
-	
+
 	cout << "running result: " << endl;
 	scheduler.Go();
 
